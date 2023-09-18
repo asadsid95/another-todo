76b5215c883b0ff8903fdee5ba24dce3ab090014
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-09-18 15:37:41 -0400

    added task removal capability

--- app.js
+++ app.js
@@ -2,19 +2,51 @@ let taskLists = document.getElementById('tasks-list')
 
 let formTaskCreation = document.getElementById('task-form')
 let inputTaskTitle = document.getElementById('task-title')
-let buttonSubmit = document.getElementById('submit')
+let buttonSubmitTask = document.getElementById('submit')
 let tasksList = document.getElementById('tasks-list')
-// adding event listener for click event so the data submitted via input field can be added into webpage
-buttonSubmit.addEventListener('click', (e) => {
 
-    e.preventDefault()
+// event listener for click event so the data submitted via input field can be added into webpage
+function addTaskToList(e){
+    
+        e.preventDefault()
+    
+        // extract text from input
+        const taskTitle = inputTaskTitle.value
+    
+        // create div for the task 
+        const divStyle = {
+            'backgroundColor': 'pink',
+            'display': 'flex',
+            'justifyContent': 'center'
+    
+        }
+    
+        const newTaskDiv = document.createElement('div')
+        newTaskDiv.className = 'task'
+        for ( const [styleName, value] of Object.entries(divStyle)) {
+            newTaskDiv.style[styleName] = value
+        }
+    
+        // create li element for the task itself
+        const newTask = document.createElement('li')
+        newTask.textContent= taskTitle
+        
+        // create button which will be beside <li>
+        const deleteTaskButton = document.createElement('button')
+        deleteTaskButton.className='delete-button'
+        deleteTaskButton.textContent = 'Delete'
+    
+        newTaskDiv.append(newTask, deleteTaskButton)
+        taskLists.append(newTaskDiv)
+        
+        inputTaskTitle.value = ''
+    
+        function removeTask() {
+            taskLists.removeChild(newTaskDiv)
+        }
 
-    const taskTitle = inputTaskTitle.value
-
-    const newTask = document.createElement('li')
-    newTask.textContent= taskTitle
-
-    taskLists.appendChild(newTask)
-
-    inputTaskTitle.value = ''
-})
+        // event listener for clicking on delete task that should delete specific task
+        deleteTaskButton.addEventListener('click', removeTask)
+    }
+    
+buttonSubmitTask.addEventListener('click', addTaskToList)
