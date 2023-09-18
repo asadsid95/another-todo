b65642dfefdbd1c706ec1c94e1b69b7b9a0c219d
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-09-18 18:01:47 -0400

    added description  ; #1 #4

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let taskLists = document.getElementById('tasks-list')
 
 let formTaskCreation = document.getElementById('task-form')
 let inputTaskTitle = document.getElementById('task-title')
+let textareaDescription = document.getElementById('task-description')
 let buttonSubmitTask = document.getElementById('submit')
 let tasksList = document.getElementById('tasks-list')
 
@@ -10,36 +11,48 @@ function addTaskToList(e){
     
         e.preventDefault()
     
-        // extract text from input
+        // extract task title from input
         const taskTitle = inputTaskTitle.value
-    
-        // create div for the task 
+        // extract task description from input
+        const taskDescription = textareaDescription.value
+
+        // create div for the task, assign class name and styles 
+        const newTaskDiv = document.createElement('div')
         const divStyle = {
             'backgroundColor': 'pink',
             'display': 'flex',
+            'flexDirection': 'column',
             'justifyContent': 'center'
     
         }
-    
-        const newTaskDiv = document.createElement('div')
-        newTaskDiv.className = 'task'
         for ( const [styleName, value] of Object.entries(divStyle)) {
             newTaskDiv.style[styleName] = value
         }
     
         // create li element for the task itself
         const newTask = document.createElement('li')
-        newTask.textContent= taskTitle
+        newTask.className='task'
+
+        // create h3 for task title
+        const newTaskTitle = document.createElement('h3')
+        newTaskTitle.textContent= taskTitle
+
+        // create p element for the task description
+        const newTaskdescription = document.createElement('p')
+        newTaskdescription.textContent= taskDescription
         
         // create button which will be beside <li>
         const deleteTaskButton = document.createElement('button')
         deleteTaskButton.className='delete-button'
         deleteTaskButton.textContent = 'Delete'
     
-        newTaskDiv.append(newTask, deleteTaskButton)
+        newTask.append(newTaskTitle, newTaskdescription, deleteTaskButton)
+
+        newTaskDiv.appendChild(newTask)
         taskLists.append(newTaskDiv)
         
         inputTaskTitle.value = ''
+        textareaDescription.value = ''
     
         function removeTask() {
             taskLists.removeChild(newTaskDiv)
@@ -48,5 +61,5 @@ function addTaskToList(e){
         // event listener for clicking on delete task that should delete specific task
         deleteTaskButton.addEventListener('click', removeTask)
     }
-    
+
 buttonSubmitTask.addEventListener('click', addTaskToList)
